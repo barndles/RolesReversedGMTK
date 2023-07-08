a61e420e46de0a324b80d8ac8fb96614c25ac7c4
barndles <barndles@gmail.com>
2023-07-07 20:46:55 -0500

    as

--- player.gd
+++ player.gd
@@ -1,9 +1,11 @@
 extends CharacterBody2D
 
-@onready var groundRay = $RayCast2D
-const SPEED = 30.0
-const maxSpeed = 200
-const JUMP_VELOCITY = -400.0
+@onready var groundRay = $Ray_DOWN
+@onready var ray_left = $Ray_LEFT
+@onready var ray_right = $Ray_RIGHT
+const SPEED = 200.0
+var direction = 1
+const JUMP_VELOCITY = -200.0
 
 # Get the gravity from the project settings to be synced with RigidBody nodes.
 var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
@@ -14,16 +16,20 @@ func _physics_process(delta):
 	if not is_on_floor():
 		velocity.y += gravity * delta
 
+	if velocity.x < 0:
+		ray_right.enabled = false
+		ray_left.enabled = true
+	elif velocity.x > 0:
+		ray_right.enabled = true
+		ray_left.enabled = false 
+	
+	velocity.x = SPEED
 	# Handle Jump.
 	if groundRay.is_colliding() == false and is_on_floor():
 		velocity.y = JUMP_VELOCITY
 
 	# Get the input direction and handle the movement/deceleration.
 	# As good practice, you should replace UI actions with custom gameplay actions.
-	var direction = Input.get_axis("ui_left", "ui_right")
-	if direction and not (velocity.x > maxSpeed or velocity.x < (maxSpeed*-1)):
-		velocity.x += direction * SPEED
-	else:
-		velocity.x = move_toward(velocity.x, 0, SPEED)
+	
 
 	move_and_slide()

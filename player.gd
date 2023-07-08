e4e419496dc519a6c6372f2624547f4ed063135c
barndles <barndles@gmail.com>
2023-07-07 21:53:55 -0500

    boy code

--- player.gd
+++ player.gd
@@ -1,35 +1,51 @@
 extends CharacterBody2D
 
-@onready var groundRay = $Ray_DOWN
-@onready var ray_left = $Ray_LEFT
-@onready var ray_right = $Ray_RIGHT
-const SPEED = 200.0
+@onready var rayR = $RAY_R
+@onready var rayL = $RAY_L
+var groundRay
+const SPEED = 100.0
 var direction = 1
-const JUMP_VELOCITY = -200.0
+const JUMP_VELOCITY = -300.0
+var vel = 0
 
 # Get the gravity from the project settings to be synced with RigidBody nodes.
 var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
 
+func _ready():
+	velocity.x = 200
 
 func _physics_process(delta):
 	# Add the gravity.
 	if not is_on_floor():
 		velocity.y += gravity * delta
 
-	if velocity.x < 0:
-		ray_right.enabled = false
-		ray_left.enabled = true
-	elif velocity.x > 0:
-		ray_right.enabled = true
-		ray_left.enabled = false 
+	if velocity.x > 0:
+		rayL.enabled = false
+		rayR.enabled = true
+		groundRay = rayR.is_colliding()
+	else:
+		rayL.enabled = true
+		rayR.enabled = false
+		groundRay = rayL.is_colliding()
 	
-	velocity.x = SPEED
-	# Handle Jump.
-	if groundRay.is_colliding() == false and is_on_floor():
+	if get_wall_normal() == Vector2(-1,0):
+		direction = -1
+	if get_wall_normal() == Vector2(1,0):
+		direction = 1
+		
+	velocity.x = SPEED * direction
+	
+	if is_on_floor() and not groundRay:
 		velocity.y = JUMP_VELOCITY
+
 
 	# Get the input direction and handle the movement/deceleration.
 	# As good practice, you should replace UI actions with custom gameplay actions.
 	
 
 	move_and_slide()
+
+func bounce(dir:Vector2):
+	
+	velocity = dir
+	

6bbaedfcaaf226e8d5c56cf9d4bcb89ec9cdc759
lukester535 <48777791+lukester535@users.noreply.github.com>
2023-07-09 00:20:44 -0500

    stupid effing print statements (Thanks alot barndles!)

--- ItemList.gd
+++ ItemList.gd
@@ -20,7 +20,6 @@ func _ready():
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
-	print(mouseOnUI)
 	mousePos = get_global_mouse_position()
 	
 	if mousePos.y > 312:

48ca6c0559e9357975feb779486bceaee8f48c56
barndles <barndles@gmail.com>
2023-07-08 22:52:51 -0500

    pause

--- Button.gd
+++ Button.gd
@@ -3,11 +3,9 @@ extends Button
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
 	if button_pressed:
-		if self.text == "RESTART":
-			get_tree().reload_current_scene()
 		get_tree().paused = true
 		self.text = "PLAY"
 	else:
 		get_tree().paused = false
-		self.text = "RESTART"
+		self.text = "PAUSED"
 	pass

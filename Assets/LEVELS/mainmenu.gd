b9a5ab14fbdf7f09177fb10ab850c83bbba4f88a
barndles <barndles@gmail.com>
2023-07-09 11:56:17 -0500

    BROKEN

--- Assets/LEVELS/mainmenu.gd
+++ Assets/LEVELS/mainmenu.gd
@@ -1,9 +1,10 @@
 extends Control
 
 @export var first_level: PackedScene
+@onready var score = $SCORE
+
 # Called when the node enters the scene tree for the first time.
-func _ready():
-	pass # Replace with function body.
+
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
--- Global.gd
+++ Global.gd
@@ -5,10 +5,12 @@ signal level_finished
 var level_over: bool = false
 
 var score: int = 0
+var final_score = 0
 
 
 func end_level():
 	print("LEVEL ENDED")
 	get_tree().paused = true
 	level_over = true
+	final_score += score
 	level_finished.emit()
